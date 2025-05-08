44d32925419de5a6e958782232d0438058af5034
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-05-08 11:03:11 +0300

    Fix Betty style: add doc and remove trailing whitespace

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - takes a pointer to an int as parameter and updates it
  * @n: taken value from main or user
  *
  * Return: nothing
@@ -10,5 +10,5 @@
 void reset_to_98(int *n)
 {
 	int x = 98;
-	*n = x;	
+	*n = x;
 }
