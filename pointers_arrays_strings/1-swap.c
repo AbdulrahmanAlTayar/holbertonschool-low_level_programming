bfa0fb9853d0ce5a0ff2c08a326808233e588a8b
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-05-08 11:26:39 +0300

    Write a function that swaps the values of two integers.

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,13 +2,13 @@
  * swap_int  - swaps the values of two integers.
  * @a: take a number from user
  * @b: same as a
- * @c: to make swap between a and b
+ *
  * Return: nothing
  */
 
 void swap_int(int *a, int *b)
 {
-    int c = *a;
-    	*a = *b;
+	int c = *a;
+	*a = *b;
 	*b = c;
 }
