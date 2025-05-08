3debd185386f01846413ee6599042535643ffd73
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-05-08 10:48:05 +0300

    assign 98 to the value pointed to by n

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using the # character
+ * @size: the size of the square
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	int x = 98;
+	*n = x;	
+}
