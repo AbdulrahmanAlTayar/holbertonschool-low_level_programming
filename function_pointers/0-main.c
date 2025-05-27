9123cc44e42a012486948d879daf44646fa0b6ce
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-05-27 10:52:53 +0300

    Implemented function pointer print_name to call a printing function on a given name

---
+++ function_pointers/0-main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+void print_name_as_is(char *name)
+{
+    printf("Hello, my name is %s\n", name);
+}
+
+void print_name_uppercase(char *name)
+{
+    unsigned int i;
+
+    printf("Hello, my uppercase name is ");
+    i = 0;
+    while (name[i])
+    {
+        if (name[i] >= 'a' && name[i] <= 'z')
+            putchar(name[i] - 32);
+        else
+            putchar(name[i]);
+        i++;
+    }
+    putchar('\n');
+}
+
+int main(void)
+{
+    print_name("Bob", print_name_as_is);
+    print_name("Bob Dylan", print_name_uppercase);
+    return (0);
+}
+
