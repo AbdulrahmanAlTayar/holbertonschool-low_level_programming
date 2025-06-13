0f4802d8b9dd5f7b15f9c5d9eedb383c61b2a36c
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-06-13 15:25:43 +0300

    fix the file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,33 +16,37 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to, r, w;
 	char buffer[1024];
 
+	/* تحقق من عدد الوسائط */
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
+	/* فتح ملف المصدر للقراءة */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
+	/* فتح ملف الهدف للكتابة (إنشاء أو اقتطاع) */
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
 
-	while ((r = read(fd_from, buffer, 1024)) > 0)
+	/* نسخ البيانات من المصدر إلى الهدف */
+	while ((r = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		w = write(fd_to, buffer, r);
 		if (w != r)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -51,21 +55,23 @@ int main(int argc, char *argv[])
 
 	if (r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
 	}
 
+	/* إغلاق ملف المصدر */
 	if (close(fd_from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 
+	/* إغلاق ملف الهدف */
 	if (close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 
